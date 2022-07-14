2b756b0ef0b5f01298475591fe65dadd0b400ab9
Eka-Saf <109159508+Eka-Saf@users.noreply.github.com>
2022-07-14 12:29:21 +0300

    Update Task3.sql

---
+++ src/test/resources/Task3.sql
@@ -0,0 +1,19 @@
+//ох и намудрила наверное, просьба не кидаться тапками..
+
+//версия №1:
+
+SELECT
+	ROUND((Orders.order_id / COUNT(Orders.order_id) OVER (PARTITION BY Promocodes.promocode_id)) * 100,2) AS persent
+FROM
+	Promocodes
+INNER JOIN
+	Orders ON Orders.promocode_id = Promocodes.promocode_id
+
+//версия №2:
+
+SELECT
+	ROUND((Orders.order_id / COUNT(Orders.order_id) OVER (PARTITION BY Promocodes.discount)) * 100,2) AS persent
+FROM
+	 Orders
+INNER JOIN
+	Promocodes ON Promocodes.promocode_id = Orders.promocode_id
